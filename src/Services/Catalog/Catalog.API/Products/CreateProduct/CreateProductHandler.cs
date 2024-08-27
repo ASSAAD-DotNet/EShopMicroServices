f0a9412b63e71cc023b5d88de2b40513b357fd15
ASSAAD EL HASSAN <coclico2017@gmail.com>
2024-08-27 16:45:46 +0200

    Add CreateProductEndpoint & CreateProductHandler

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,35 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name,
+        string Description,
+        List<string> Category,
+        string ImageFile,
+        decimal Price)
+        : ICommand<CreateProductResult>;
+    
+    public record CreateProductResult(Guid Id);
+
+    public class CreateProductCommandHandler :
+        ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            //Business logique to create Product
+            var product = new Product()
+            {
+                Name = request.Name,
+                Description = request.Description,
+                Category = request.Category,
+                ImageFile = request.ImageFile,
+                Price = request.Price,
+            };
+            //Save to the data base
+
+            //Return result
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}

c4ed33f165fde09bfae2ef8210a09dbfe5bceb80
ASSAAD EL HASSAN <coclico2017@gmail.com>
2024-08-28 16:11:33 +0200

    Add FluentValidation To the CreateProductCommand

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -7,12 +7,31 @@ public record CreateProductCommand(string Name,
     : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
+public class CreateProductCommandValidaitor 
+    : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidaitor()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("Image file is required");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("The price must be greater then 0");
+    }
+}
+
 internal class CreateProductCommandHandler
-    (IDocumentSession session)
+    (IDocumentSession session,IValidator<CreateProductCommand> validator)
     :ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        var validate = await validator.ValidateAsync(command);
+        var errors = validate.Errors.Select(x => x.ErrorMessage).ToList();
+        if (errors.Any())
+        {
+            throw new ValidationException(errors.FirstOrDefault());
+        }
         //Business logique to create Product
         var product = new Product()
         {

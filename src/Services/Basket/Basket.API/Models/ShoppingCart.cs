2ff8706ec8278925be3abfd49128624f8ffb5658
ASSAAD EL HASSAN <coclico2017@gmail.com>
2024-08-30 15:20:26 +0200

    Add basket User case Feature handler and endpoints

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.API.Models;
+public class ShoppingCart
+{
+    public string UseerName { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = new();
+    public decimal TotalPrice => Items.Sum(x=> x.Price* x.Quantity);
+
+    public ShoppingCart(string userName)
+    {
+        UseerName = userName;
+    }
+    public ShoppingCart()
+    {
+        
+    }
+}

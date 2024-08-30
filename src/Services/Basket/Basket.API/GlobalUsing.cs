97f722db9f45464b4d6a954f65f2c4d5c136166f
ASSAAD EL HASSAN <coclico2017@gmail.com>
2024-08-30 16:45:57 +0200

    Add Dependenecy injection for de carter and MediatR

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -4,3 +4,4 @@ global using MediatR;
 global using Basket.API.Models;
 global using BuildingBlocks.CQRS;
 global using FluentValidation;
+global using BuildingBlocks.Behaviors;
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(conf =>
+{
+    conf.RegisterServicesFromAssembly(assembly);
+    conf.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    conf.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
 var app = builder.Build();
 
+
+app.MapCarter();
 
 app.Run();

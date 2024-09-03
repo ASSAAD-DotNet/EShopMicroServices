e6a2c178de8874f20c08bbfdd92e96d4aa34be08
ASSAAD-DotNet <assaadelhassan@gmail.com>
2024-09-03 09:52:03 +0200

    Add Healthcheck & DockerFile  & Docker-compose (#8)
    * Add Scrutor Decorate For CachedBasketRepository
    
    * Add Redis Cache in the docker compose
    
    * Add HealthCheck for Basket Api
    
    * Add Dockerfile and Docker-compose for basket api
    
    ---------
    
    Co-authored-by: ASSAAD EL HASSAN <coclico2017@gmail.com>

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -11,3 +11,8 @@ global using System.Threading.Tasks;
 global using BuildingBlocks.Exceptions;
 global using Basket.API.Exceptions;
 global using Basket.API.Data;
+global using System.Text.Json;
+global using Microsoft.Extensions.Caching.Distributed;
+global using BuildingBlocks.Exceptions.Handler;
+global using HealthChecks.UI.Client;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,3 @@
-using BuildingBlocks.Exceptions.Handler;
-
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly;
@@ -16,14 +14,31 @@ builder.Services.AddMarten(options => {
     options.Schema.For<ShoppingCart>().Identity(c => c.UserName);
 }).UseLightweightSessions();
 
+
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(Option =>
+{
+    Option.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("DataBase")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
+
 
 var app = builder.Build();
 
 
 app.MapCarter();
 app.UseExceptionHandler(option => { });
+app.MapHealthChecks("/Health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();

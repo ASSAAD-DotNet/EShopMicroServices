a00ef25aee0f4898c57571d59075006d669a3e72
ASSAAD EL HASSAN <coclico2017@gmail.com>
2024-09-03 13:39:29 +0200

    Add DiscountProtoService

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountProtoService.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+using static Discount.Grpc.DiscountProtoService;
+
+namespace Discount.Grpc.Services;
+
+public class DiscountProtoService : DiscountProtoServiceBase
+{
+    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        return base.GetDiscount(request, context);
+    }
+
+    public override Task<CouponModel> CreateDiscoun(CreateDiscounRequest request, ServerCallContext context)
+    {
+        return base.CreateDiscoun(request, context);
+    }
+
+    public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        return base.UpdateDiscount(request, context);
+    }
+
+    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        return base.DeleteDiscount(request, context);
+    }
+}

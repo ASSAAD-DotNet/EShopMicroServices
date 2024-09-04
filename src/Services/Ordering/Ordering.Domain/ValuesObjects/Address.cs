2e4483109e36260bed65d14cad2f9bb65677294a
ASSAAD EL HASSAN <coclico2017@gmail.com>
2024-09-04 16:04:19 +0200

    Add abstractions & Models  & valuesObjects

---
+++ src/Services/Ordering/Ordering.Domain/ValuesObjects/Address.cs
@@ -0,0 +1,20 @@
+﻿namespace Ordering.Domain.ValuesObjects;
+
+//public record Address (string FirstName,
+//    string LastName,
+//    string EmailAdress,
+//    string AddressLine,
+//    string Country,
+//    string State,
+//    string ZipCode)
+public record Address
+{
+    public string FirstName { get; } = default!;
+    public string LastName { get; } = default!;
+    public string? EmailAdress { get; } = default!;
+    public string AddressLine { get; } = default!;
+    public string Country { get; } = default!;
+    public string State { get; } = default!;
+    public string ZipCode { get; } = default!;
+}
+

5c110d626ee206775721f382cae4927e33c49c8f
ASSAAD EL HASSAN <coclico2017@gmail.com>
2024-09-17 11:10:54 +0200

    Add MassTransit RabbitMQ & configuration

--- src/BuildingBlocks.Messaging/Events/IntegrationEvent.cs
+++ src/BuildingBlocks.Messaging/Events/IntegrationEvent.cs
@@ -3,5 +3,5 @@ public record IntegrationEvent
 {
     public Guid Id => Guid.NewGuid();
     public DateTime OnOccurredOn => DateTime.UtcNow;
-    public string EventType => GetType().AssemblyQualifiedName;
+    public string EventType => GetType().AssemblyQualifiedName!;
 }

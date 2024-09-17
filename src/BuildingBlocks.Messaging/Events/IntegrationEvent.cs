eb52c4b2cbc74c0638751c78a7f671c02e6e3691
ASSAAD EL HASSAN <coclico2017@gmail.com>
2024-09-17 11:02:02 +0200

    Add BasketCheckoutEevent & IntegrationEvent

---
+++ src/BuildingBlocks.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildingBlocks.Messaging.Events;
+public record IntegrationEvent
+{
+    public Guid Id => Guid.NewGuid();
+    public DateTime OnOccurredOn => DateTime.UtcNow;
+    public string EventType => GetType().AssemblyQualifiedName;
+}

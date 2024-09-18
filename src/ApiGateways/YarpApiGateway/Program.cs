0c60788ed0787ba9886829ebf4c738336b587529
ASSAAD EL HASSAN <coclico2017@gmail.com>
2024-09-18 14:19:30 +0200

    Add Test to the YarpApiGateway

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,9 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add servcies to the container.
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
 // Configuration the http request pipline
+app.MapReverseProxy();
 
 app.Run();
